98ef37fa5518e3f9a40c2cf3a72b23fc3f7b6aae
esski <114247177+esski@users.noreply.github.com>
2023-01-25 20:29:13 -0500

    edit zoom level

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,
+    zoom: 7,
     center: { lat: 43.651070, lng: -79.347015 },
   });
   // Create an array of alphabetical characters used to label the markers.
